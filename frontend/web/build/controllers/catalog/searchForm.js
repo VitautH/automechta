95469234711f3eaaf26b4a4f0e56ed415b8bea81
Григорович Витовт <ip-94@ya.ru>
2017-10-31 09:59:45 +0300

    Implemented task 49 (min and max year in SearchForm)

--- frontend/web/build/controllers/catalog/searchForm.js
+++ frontend/web/build/controllers/catalog/searchForm.js
@@ -19,8 +19,8 @@ define(['jquery', 'application', 'preloader', 'urijs/URI'], function ($, applica
         uri.setQuery('ProductSearchForm[type]', $(this).val());
         preloader.show($form);
         $.ajax({
-            url : uri.toLocaleString(),
-            success : function (response) {
+            url: uri.toLocaleString(),
+            success: function (response) {
                 var $result = $('<div>' + response + '</div>');
                 $('.js-vehicle-type-specs').replaceWith($result.find('.js-vehicle-type-specs'));
                 preloader.hide($form);
@@ -40,9 +40,24 @@ define(['jquery', 'application', 'preloader', 'urijs/URI'], function ($, applica
             }
         });
 
-        if ($('[name="ProductSearchForm[yearTo]"]').val() < from) {
-            $('[name="ProductSearchForm[yearTo]"]').val(from);
+        if ($('[name="ProductSearchForm[yearTo]"]').val() < from || $('[name="ProductSearchForm[yearTo]"]').val() == from) {
+            $('[name="ProductSearchForm[yearTo]"] option:eq(1)').attr('selected', 'selected');
         }
+
+    });
+    $('[name="ProductSearchForm[yearTo]"]').on('change', function () {
+        var to = $(this).val();
+        $('[name="ProductSearchForm[yearFrom]"] option').removeAttr('disabled').each(function () {
+
+            if ($('[name="ProductSearchForm[yearFrom]"]').attr('value') > to) {
+                $('[name="ProductSearchForm[yearFrom]"]').attr('disabled', 'disabled');
+            }
+        });
+
+        if ($('[name="ProductSearchForm[yearFrom]"]').val() < to || $('[name="ProductSearchForm[yearFrom]"]').val() == to) {
+            $('[name="ProductSearchForm[yearFrom]"] option:last').attr('selected', 'selected');
+        }
+
     });
 
     $('.js-reset-form').on('click', function () {
@@ -56,14 +71,14 @@ define(['jquery', 'application', 'preloader', 'urijs/URI'], function ($, applica
         var uri = currentUri.clone();
         var data = $form.serialize();
         if (currentParams.tableView !== undefined) {
-            data += '&tableView='+ currentParams.tableView;
+            data += '&tableView=' + currentParams.tableView;
         }
 
         $.ajax({
-            url : currentUri.path(),
-            data : data,
-            success : function (response) {
-                var $response = $('<div>'+response+'</div>');
+            url: currentUri.path(),
+            data: data,
+            success: function (response) {
+                var $response = $('<div>' + response + '</div>');
                 $('.js-product-list').replaceWith($response.find('.js-product-list'));
                 $('.b-pageHeader__search').replaceWith($response.find('.b-pageHeader__search'));
                 preloader.hide($('.js-product-list'));
@@ -74,16 +89,16 @@ define(['jquery', 'application', 'preloader', 'urijs/URI'], function ($, applica
     });
 
     function updateModelsList(makeId) {
-        if (makeId){
+        if (makeId) {
             preloader.show($form);
             $.ajax({
-                url : '/api/productmake/models?makeId=' + makeId,
-                success : function (response) {
+                url: '/api/productmake/models?makeId=' + makeId,
+                success: function (response) {
                     $('[name="ProductSearchForm[model]"]').empty();
                     var $option = $(' <option value="">Любой</option>');
                     $('[name="ProductSearchForm[model]"]').append($option);
                     $.each(response, function (key, val) {
-                        var $option = $('<option value="' + key + '">'+ key +'</option>');
+                        var $option = $('<option value="' + key + '">' + key + '</option>');
                         $('[name="ProductSearchForm[model]"]').append($option);
                     });
                     preloader.hide($form);
@@ -101,20 +116,20 @@ define(['jquery', 'application', 'preloader', 'urijs/URI'], function ($, applica
         if (type) {
             preloader.show($form);
             $.ajax({
-                url : '/api/productmake/makers?type=' + type,
-                dataType : 'json',
-                success : function (response) {
+                url: '/api/productmake/makers?type=' + type,
+                dataType: 'json',
+                success: function (response) {
                     $makeSelect.empty();
                     var $option = $('<option value="">Любая</option>');
                     $makeSelect.append($option);
                     $.each(response, function (key, val) {
-                        var $option = $('<option value="' + key + '">'+ val +'</option>');
+                        var $option = $('<option value="' + key + '">' + val + '</option>');
                         $makeSelect.append($option);
                     });
                     preloader.hide($form);
                     $makeSelect.trigger('change');
                 },
-                error : function () {
+                error: function () {
                     preloader.hide($form);
                     $makeSelect.replaceWith($('<span>Error</span>'));
                 }
